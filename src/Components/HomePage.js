be36e0b0e3934940d47ae4101642d43ef81db2ec
Vishal <vishal.pugazhendhi@gmail.com>
2023-04-28 03:11:03 -0700

    deploy fail

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import CreatePrivateGame from './CreatePrivateGame';
 import JoinPrivateGame from './JoinPrivateGame';
 import { useNavigate } from 'react-router-dom';
-import PrivateGameServices from '../Services/PrivateGameServices';
+import PrivateGameServices from '../Services/PrivateGameServices.js';
 
 const Homepage = () => {
     const navigate = useNavigate();
